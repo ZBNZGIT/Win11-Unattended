063bff32174bab940c0d37f036284942d77de35f
ZBNZ <68793343+ZBNZGIT@users.noreply.github.com>
2025-03-09 18:08:09 +1300

    Update ScriptsLauncher.ps1

--- ScriptsLauncher.ps1
+++ ScriptsLauncher.ps1
@@ -1,40 +1,13 @@
-$Host.UI.RawUI.WindowTitle = "Scripts Launcher"
-$Host.UI.RawUI.WindowSize = New-Object System.Management.Automation.Host.Size(54, 6)
-
-$scripts = @{
-    "Install Apps" = "https://raw.githubusercontent.com/ZBNZGIT/Win11-Unattended-Scripts/main/Scripts/AppsInstallerChocolatey.bat"
-    "Remove Edge" = "https://raw.githubusercontent.com/ZBNZGIT/Win11-Unattended-Scripts/main/Scripts/RemoveEdge.ps1"
-    "Activate Windows" = "https://get.activated.win"
-}
-
-$scriptNames = @("Install Apps", "Remove Edge", "Activate Windows")
-$index = 0
-$confirm = $false
-
-[System.Console]::CursorVisible = $false
-
-function Show-Menu {
-    $Host.UI.RawUI.BackgroundColor = 'Black'
-    $Host.UI.RawUI.ForegroundColor = 'Green'
-    Clear-Host
-
-    Write-Host "Select an option (Use Arrow keys, Enter to confirm)" -ForegroundColor White
-
-    for ($i = 0; $i -lt $scriptNames.Count; $i++) {
-        if ($i -eq $index) {
-            Write-Host "> $($scriptNames[$i])" -BackgroundColor White -ForegroundColor Black
-        } else {
-            Write-Host "  $($scriptNames[$i])" -ForegroundColor White
-        }
-    }
-}
+$InstallApps = "https://raw.githubusercontent.com/ZBNZGIT/Win11-Unattended-Scripts/main/Scripts/AppsInstallerChocolatey.bat"     
+$RemoveEdge = "https://raw.githubusercontent.com/ZBNZGIT/Win11-Unattended-Scripts/main/Scripts/RemoveEdge.ps1" 
+$MASActivation = "https://get.activated.win"
 
 function Run-Script {
     param (
-        [string]$scriptUrl
+        [string]$scriptUrl,
+        [string]$extension = ".ps1"
     )
     try {
-        $extension = if ($scriptUrl -match "\.bat$") { ".bat" } else { ".ps1" }
         $scriptContent = Invoke-RestMethod -Uri $scriptUrl
         $tempFile = [System.IO.Path]::GetTempFileName() + $extension
         Set-Content -Path $tempFile -Value $scriptContent
@@ -42,7 +15,13 @@ function Run-Script {
         if ($extension -eq ".bat") {
             Start-Process -FilePath "cmd.exe" -ArgumentList "/c $tempFile" -WindowStyle Normal
         } else {
-            Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File $tempFile" -WindowStyle Hidden
+            if ($scriptUrl -eq $MASActivation) {
+                # Hide PowerShell window for MAS Activation
+                Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File $tempFile" -WindowStyle Hidden
+            } else {
+                # Show PowerShell window for other scripts
+                Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File $tempFile" -WindowStyle Normal
+            }
         }
     }
     catch {
@@ -50,22 +29,25 @@ function Run-Script {
     }
 }
 
-
-Show-Menu
-
-while (-not $confirm) {
-    $key = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown").VirtualKeyCode
-    switch ($key) {
-        38 { if ($index -gt 0) { $index-- } }
-        40 { if ($index -lt $scriptNames.Count - 1) { $index++ } }
-        13 { $confirm = $true }
-    }
-    Show-Menu
+function Show-Menu {
+    Clear-Host
+    Write-Host @"
+1. Install Apps
+2. Remove Edge
+3. Activate Windows
+4. Exit
+"@
 }
 
-[System.Console]::CursorVisible = $true
-
-Clear-Host
-Run-Script -scriptUrl $scripts[$scriptNames[$index]]
-Start-Sleep -Seconds 2
-exit
+do {
+    Show-Menu
+    $choice = Read-Host "Please select an option (1-4)"
+    
+    switch ($choice) {
+        '1' { Run-Script -scriptUrl $InstallApps -extension ".bat"; exit }
+        '2' { Run-Script -scriptUrl $RemoveEdge -extension ".ps1"; exit }
+        '3' { Run-Script -scriptUrl $MASActivation -extension ".ps1"; exit }
+        '4' { exit }
+        default { Write-Host "Invalid choice, please try again." -ForegroundColor Red }
+    }
+} while ($true)
