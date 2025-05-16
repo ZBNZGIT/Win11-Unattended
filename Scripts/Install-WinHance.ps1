f7c18660f19b590dc6580e04b78c9df0010aef22
ZBNZ <68793343+ZBNZGIT@users.noreply.github.com>
2025-05-16 15:17:53 +1200

    Create Install-WinHance.ps1

---
+++ Scripts/Install-WinHance.ps1
@@ -0,0 +1,5 @@
+$installerPath = "$env:TEMP\Winhance.Installer.exe"
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile("https://github.com/memstechtips/Winhance/releases/latest/download/Winhance.Installer.exe", $installerPath)
+Start-Process -FilePath $installerPath
+exit
